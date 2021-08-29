155975d138a033f8c85c6d09980452667086127f
siamtbhuiyan <siamtbhuiyan@gmail.com>
2021-08-29 23:55:05 +0600

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123-2843839" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const [search, setSearch] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -31,10 +36,21 @@ const App = () => {
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
   };
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    const regularExpression = new RegExp(search, "i");
+    const filteredPersons = () =>
+      persons.filter((person) => person.name.match(regularExpression));
+    setPersons(filteredPersons);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <input value={search} onChange={handleSearchChange} />
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
