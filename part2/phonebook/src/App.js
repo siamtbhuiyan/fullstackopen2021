28faf69e812e067d63acd23d679511c6231d8e1f
siamtbhuiyan <siamtbhuiyan@gmail.com>
2021-08-31 04:41:43 +0600

    Exercise 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -28,7 +28,12 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(newPerson));
+
+      axios
+        .post("http://localhost:3001/persons", newPerson)
+        .then((response) => {
+          setPersons(persons.concat(response.data));
+        });
     }
 
     setNewName("");
