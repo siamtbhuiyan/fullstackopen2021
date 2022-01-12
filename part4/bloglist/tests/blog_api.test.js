e26220e4ded16377737261910c4d320972fe5b1f
siamtbhuiyan <siamtbhuiyan@gmail.com>
2022-01-12 15:07:15 +0600

    Exercise 4.11

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,6 +5,7 @@ const helper = require('../utils/list_helper')
 const api = supertest(app)
 
 const Blog = require('../models/blog.js')
+const { application } = require('express')
 
 beforeEach (async () => {
     await Blog.deleteMany({})
@@ -35,8 +36,7 @@ test('a blog can be added', async () => {
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
+        likes: 5
       }
     await api
         .post('/api/blogs')
@@ -49,6 +49,23 @@ test('a blog can be added', async () => {
     expect(blogsAtTheEnd).toHaveLength(helper.blogs.length + 1)
     const content = blogsAtTheEnd.map(b => b.title)
     expect(content).toContain('Go To Statement Considered Harmful')
+})
+
+test('likes is 0 if there is no likes property in the request', async () => {
+    const newBlog = {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+
+    const blogs = await Blog.find({})
+    const blogsJSON = blogs.map(b => b.toJSON())
+    const length = helper.blogs.length
+    expect(blogsJSON[length].likes).toBe(0)
 })
 
 afterAll(() => {
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,43 +3,37 @@ const blogs = [
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
+      likes: 7
     },
     {
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
+      likes: 5
     },
     {
       title: "Canonical string reduction",
       author: "Edsger W. Dijkstra",
       url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
+      likes: 12
     },
     {
       title: "First class tests",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
+      likes: 10
     },
     {
       title: "TDD harms architecture",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
+      likes: 0
     },
     {
       title: "Type wars",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
+      likes: 2
     }
 ]
   
@@ -48,8 +42,7 @@ const blogs = [
       title: 'Go To Statement Considered Harmful',
       author: 'Edsger W. Dijkstra',
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
+      likes: 5
     }
   ]
 
