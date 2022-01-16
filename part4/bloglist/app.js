dc0bad9e6d6f592d15574dadc64d22d61a6b3138
siamtbhuiyan <siamtbhuiyan@gmail.com>
2022-01-16 15:49:15 +0600

    Exercise 4.20

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -8,7 +8,7 @@ const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users.js')
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger.js')
-const errorHandler = require('./utils/middleware.js')
+const middleware = require('./utils/middleware.js')
 
 
 mongoose.connect(config.MONGODB_URI)
@@ -21,11 +21,12 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-app.use(errorHandler)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,12 +1,25 @@
 const logger = require('./logger.js')
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      request.token = authorization.substring(7)
+    }
+    
+    next()
+}
+
 const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
     if (error.name === 'ValidationError') {
         response.status(400).send({ error: error.message })
+    } else if (error.name == 'JsonWebTokenError') {
+        response.status(401).send({ error: 'token missing or invalid' })
     }
 
     next(error)
 }
 
-module.exports = errorHandler
+module.exports = {
+    errorHandler, tokenExtractor
+}
