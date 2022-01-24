5a29ea29aad3c5140ddee574c2cea5ce284985ef
siamtbhuiyan <siamtbhuiyan@gmail.com>
2022-01-24 17:23:53 +0600

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,63 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    <li>
-    {blog.title} {blog.author}
-    </li>
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+  const [visibility, setVisibility] = useState(false)
+  let btnName = 'view'
+
+  const handleVisibility = (e) => {
+    e.preventDefault()
+    setVisibility(!visibility)
+  }
+
+  if (visibility) {
+    btnName = 'hide'
+  } else {
+    btnName = 'view'
+  }
+
+  const blogStyle = {
+    listStyle: "none",
+    padding: "20px",
+    backgroundColor: "#ddd",
+    margin: "10px 0",
+    fontSize: "20px"
+  }
+
+  const btnStyle = {
+    marginLeft: "8px",
+  }
+
+  if (!visibility) {
+    return (
+    
+      <div style={blogStyle}>
+        <li>
+        Blog: {blog.title} {blog.author}
+        <button style={btnStyle} onClick={handleVisibility}>{btnName}</button>
+        </li>
+      </div>
+    )
+  } else {
+    return (
+    
+      <div style={blogStyle}>
+        <li>
+        Blog: {blog.title} {blog.author}
+        <button style={btnStyle} onClick={handleVisibility}>{btnName}</button>
+        </li>
+        <li>
+          Link: {blog.url}
+        </li>
+        <li>
+          Likes: {blog.likes}
+          <button style={btnStyle}>Like</button>
+        </li>
+        <li>
+          User: {blog.user.name}
+        </li>
+      </div>
+    )
+  }
+
+  }
 
 export default Blog
